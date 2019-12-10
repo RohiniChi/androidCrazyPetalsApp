5c92042d41204a73ee7b2424cefdb17c357ad14f
Prajakta Ukande <prajakta@plug-able.com>
2019-12-10 14:50:01 +0530

    Mcom-1186 (CP) Android->About Us-> Required loader on a about us screen

--- app/src/main/java/com/plugable/mcommerceapp/cpmvp1/ui/fragments/AboutUsFragment.kt
+++ app/src/main/java/com/plugable/mcommerceapp/cpmvp1/ui/fragments/AboutUsFragment.kt
@@ -1,26 +1,41 @@
 package com.plugable.mcommerceapp.cpmvp1.ui.fragments
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import com.mixpanel.android.mpmetrics.MixpanelAPI
 import com.plugable.mcommerceapp.cpmvp1.R
-import com.plugable.mcommerceapp.cpmvp1.utils.constants.IntentFlags
+import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.fragment_about_us.*
 import kotlinx.android.synthetic.main.layout_common_toolbar.*
-import org.json.JSONObject
 
-class AboutUsFragment:Fragment() {
+
+class AboutUsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-
+//        setThemeToComponents()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    /*private fun setThemeToComponents() {
+        progressBarAboutUs.indeterminateDrawable.setColorFilter(
+            Color.BLACK,
+            PorterDuff.Mode.MULTIPLY
+        )
+    }*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about_us, container, false)
     }
@@ -35,12 +50,26 @@ class AboutUsFragment:Fragment() {
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initializeViews() {
-        webViewAboutUS.settings.javaScriptEnabled=true
+        webViewAboutUS.settings.javaScriptEnabled = true
         webViewAboutUS.loadUrl(getString(R.string.about_us_web_url))
 
+        progressBarAboutUs.indeterminateDrawable.setColorFilter(
+            Color.BLACK,
+            PorterDuff.Mode.MULTIPLY
+        )
 //        mixPanel = MixpanelAPI.getInstance(context, resources.getString(R.string.mix_panel_token))
 //        sendMixPanelEvent()
+        webViewAboutUS.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBarAboutUs.show()
+            }
 
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBarAboutUs.hide()
+            }
+        }
     }
 
 /*
@@ -58,9 +87,9 @@ class AboutUsFragment:Fragment() {
 
     }
 
-   /* override fun onDestroy() {
-        mixPanel.flush()
-        super.onDestroy()
-    }
-*/
+    /* override fun onDestroy() {
+         mixPanel.flush()
+         super.onDestroy()
+     }
+ */
 }
