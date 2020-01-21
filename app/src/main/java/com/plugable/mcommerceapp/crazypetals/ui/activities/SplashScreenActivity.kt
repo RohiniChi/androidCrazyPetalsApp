16e2aa49a93b1b02d2d375c8760562ada3510c12
Prajakta Ukande <prajakta@plug-able.com>
2020-01-21 11:30:06 +0530

    Released production build for playstore release.

--- app/src/main/java/com/plugable/mcommerceapp/crazypetals/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/plugable/mcommerceapp/crazypetals/ui/activities/SplashScreenActivity.kt
@@ -423,7 +423,7 @@ class SplashScreenActivity : AppCompatActivity(), View.OnClickListener {
             "PA003",
             "CrazyPetals"
         )
-        App.HostUrl = apiConfigData.devBaseUrl
+        App.HostUrl = apiConfigData.prodBaseUrl
         SharedPreferences.getInstance(this@SplashScreenActivity).hostUrl =
             App.HostUrl
         SharedPreferencesForBaseUrl.getInstance(this).tokenDataPreference = apiConfigData
